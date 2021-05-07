54881163e3a3fac872aca1b4a82f49983f5ed4c5
Faustas <faustux99@gmail.com>
2021-05-07 20:44:45 +0100

    first commit

---
+++ Auto_Parts/web/testing.php
@@ -0,0 +1,11 @@
+
+<html>
+   <body>
+   
+      <form action = "/Auto_Parts/web/testing2.php" method = "GET">
+      <form action = "/Auto_Parts/web/ssss" method = "GET">
+
+      </form>
+      
+   </body>
+</html>
