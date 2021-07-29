ffc878cc7dff68b6bea15aae2abd372fb6d6474f
faustas99 <83924432+faustas99@users.noreply.github.com>
2021-07-29 17:55:04 +0100

    Delete WebDev_CV directory

--- WebDev_CV/learning/action.php
+++
@@ -1,7 +0,0 @@
-<?php
-$message = "";
-if(isset($_POST['SubmitButton'])){ //check if form was submitted
-  $input = $_POST['inputText']; //get input text
-  $message = "Success! You entered: ".$input;
-}    
-?>
